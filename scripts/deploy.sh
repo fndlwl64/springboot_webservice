552ced2e1999bf371d943a17a435305e3f1875ab
fndlwl64 <fndlwl64@gmail.com>
2023-04-11 20:09:02 +0900

    modify codedeploy config(chmod +x scripts/*.sh)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,11 @@ PROJECT_NAME=/springboot_webservice
 
 echo "> Build 파일복사"
 
-cp $REPOSITORY/zip/*.jar $PROJECT_NAME/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl springboot_webservice | grep jar | awk'{print $1}')
+CURRENT_PID=$(pgrep -fl springboot_webservice | grep jar | awk '{print $1}')
 
 echo "> 현재 구동 중인 애플리케이션 pid: ${CURRENT_PID}"
 
@@ -23,7 +23,7 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr ${REPOSITORY}/zip/*.jar | grep -v "plain"| tail -n 1)
+JAR_NAME=$(ls -tr ${REPOSITORY}/*.jar | grep -v "plain"| tail -n 1)
 
 echo "> JAR Name: ${JAR_NAME}"
 

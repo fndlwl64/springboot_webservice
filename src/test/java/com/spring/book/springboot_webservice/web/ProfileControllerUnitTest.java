35546d668064406ad634c8f01b0c2e7eb0c158f4
fndlwl64 <fndlwl64@gmail.com>
2023-04-12 18:20:28 +0900

    create ProfileController

--- src/test/java/com/spring/book/springboot_webservice/web/ProfileControllerUnitTest.java
+++ src/test/java/com/spring/book/springboot_webservice/web/ProfileControllerUnitTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ProfileControllerUnitTest {
 
     @Test
-    public void real_profile이_조회된다(){
+    public void real_profile_select(){
         //given
         String expectedProfile = "real";
         MockEnvironment env = new MockEnvironment();
@@ -16,6 +16,38 @@ public class ProfileControllerUnitTest {
         env.addActiveProfile("oauth");
         env.addActiveProfile("real-db");
 
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void real_profile_if_first_not_exist(){
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void active_profile_if_not__default_select(){
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
         ProfileController controller = new ProfileController(env);
 
         //when
